0660678c80cda037d6dcbeb639162934cf719307
Vikas <vikaskumar07.dev@gmail.com>
2023-03-29 17:01:44 +0530

    modal for delete permission

--- resources/js/Pages/Admin/Permission/PermissionIndex.vue
+++ resources/js/Pages/Admin/Permission/PermissionIndex.vue
@@ -1,12 +1,32 @@
 <script setup>
 import AdminLayout from '@/Layouts/AdminLayout.vue';
-import { Head, Link } from '@inertiajs/vue3';
+import { Head, Link, useForm } from '@inertiajs/vue3';
 import Table from '@/Components/Table.vue';
 import TableRow from '@/Components/TableRow.vue';
 import TableHeaderCell from '@/Components/TableHeaderCell.vue';
 import TableDataCell from '@/Components/TableDataCell.vue';
+import Modal from '@/Components/Modal.vue';
+import DangerButton from '@/Components/DangerButton.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import { ref } from 'vue';
 
 defineProps(['permissions']);
+const form = useForm({});
+const showConfirmPermissionModal = ref(false);
+
+const confirmDeletePermission = () => {
+    showConfirmPermissionModal.value = true;
+}
+
+const deletePermission = (id) => {
+    form.delete(route('permissions.destroy', id), {
+        onSuccess: () => closeModal()
+    });
+}
+
+const closeModal = () => {
+    showConfirmPermissionModal.value = false;
+}
 </script>
 
 <template>
@@ -33,7 +53,17 @@ defineProps(['permissions']);
                             <TableHeaderCell>{{ permission.name }}</TableHeaderCell>
                             <TableHeaderCell class="space-x-4">
                                 <Link :href="route('permissions.edit', permission.id)" class="text-green-400 hover:text-green-600">Edit</Link>
-                                <Link :href="route('permissions.destroy', permission.id)" method="DELETE" as="button" class="text-red-400 hover:text-red-600">Delete</Link>
+                                
+                                <button @click="confirmDeletePermission" class="text-red-400 hover:text-red-600">Delete</button>
+                                <Modal :show="showConfirmPermissionModal" @close="closeModal">
+                                    <div class="p-6">
+                                        <h2 class="text-lg font-semibold text-slate-800">Are you sure to delete this permission?</h2>
+                                        <div class="mt-6 flex space-x-4">
+                                            <DangerButton @click="$event => deletePermission(permission.id)">Delete</DangerButton>
+                                            <SecondaryButton @click="closeModal">Cancel</SecondaryButton>
+                                        </div>
+                                    </div>
+                                </Modal>
                             </TableHeaderCell>
                         </TableRow>
                     </template>
